6852feccdc6a67726e18ea7ebe098e73d210ec52
AungMoeMyintwymm <https://github.com/AungMoeMyintwymm/AungMoeMyintwymm.git>
2021-12-04 09:38:33 +0630

    1 commit

---
+++ clinic/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Clinic
+
+# Register your models here.
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ['patient_name', 'patient_phone', 'patient_address', 'patient_case', 'drug', 'fees', 'doctor', 'datetime']
+    list_filter = ['patient_name', 'patient_address', 'patient_case', 'doctor', 'datetime']
+    search_field =['patient_name']
+admin.site.register(Clinic, ClinicAdmin)
